6d91e5438a955e8c44fe8df1d7cd3cb456ae4328
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2017-11-05 05:44:33 -0500

    mutex protected the bus

--- src/phys.cpp
+++ src/phys.cpp
@@ -1,9 +1,11 @@
 #include "mraa.h"
 #include "phys.hpp"
 #include <stdint.h>
+#include <pthread.h>
 
 static mraa_gpio_context led_gpio;
 static mraa_i2c_context i2c0;
+static pthread_mutex_t i2c_mutex;
 
 static uint8_t rx_tx_buffer[I2C_BUFFER_MAX];
 
@@ -20,6 +22,7 @@ void phys_init()
   mraa_init();
   led_gpio = mraa_gpio_init(LED_PIN);
   mraa_gpio_dir(led_gpio, MRAA_GPIO_OUT);
+  pthread_mutex_init(&i2c_mutex, NULL);
 
   //init i2C bus
   i2c0 = mraa_i2c_init(I2C_BUS);
@@ -74,16 +77,22 @@ bool phys_i2c_ping(uint8_t address)
 //write 1 bytes to the bus
 mraa_result_t phys_i2c_write(uint8_t address, uint8_t data)
 {
+  pthread_mutex_lock(&i2c_mutex);
   mraa_i2c_address(i2c0, address);
   rx_tx_buffer[0] =  data;
-  return mraa_i2c_write(i2c0, rx_tx_buffer, 1);
+  mraa_result_t r = mraa_i2c_write(i2c0, rx_tx_buffer, 1);
+  pthread_mutex_unlock(&i2c_mutex);
+  return r;
 }
 
 //read 1 byte from the bus
 uint8_t phys_i2c_read_1(uint8_t address)
 {
+  pthread_mutex_lock(&i2c_mutex);
   mraa_i2c_address(i2c0, address);
-  return (uint8_t) mraa_i2c_read_byte(i2c0);
+  uint8_t r = (uint8_t)mraa_i2c_read_byte(i2c0)
+  pthread_mutex_unlock(&i2c_mutex);
+  return r;
 }
 
 //read 2 bytes from the bus (MSB first)
@@ -92,12 +101,15 @@ uint16_t phys_i2c_read_2(uint8_t address)
   int r;
   uint16_t result;
   printf("Reading 2 bytes: ");
+  
+  pthread_mutex_lock(&i2c_mutex);
   mraa_i2c_address(i2c0, address);
   r = mraa_i2c_read(i2c0, rx_tx_buffer, 2);
   if(r == 2)
     result = (rx_tx_buffer[0] << 8) + rx_tx_buffer[1];
   else
     result = ERR; //make this more visible
+  pthread_mutex_unlock(&i2c_mutex);
 
   printf("Results = %d\n", result);
   return result;
